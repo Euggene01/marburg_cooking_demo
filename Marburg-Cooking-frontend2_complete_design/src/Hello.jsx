9d5b01c985bd9477f6945b8c324d03d6cba22379
Euggene01 <156249252+Euggene01@users.noreply.github.com>
2026-01-15 13:29:41 +0100

    Delete Marburg-Cooking-frontend2_complete_design directory

--- Marburg-Cooking-frontend2_complete_design/src/Hello.jsx
+++
@@ -1,5 +0,0 @@
-function Hello() {
-  return <h1>Hallo Marburg kocht!</h1>;
-}
-
-export default Hello;
--- Marburg-Cooking-frontend2_complete_design/src/config/api.js
+++
@@ -1,8 +0,0 @@
-export const API_URL = import.meta.env.VITE_API_URL;
-
-export const ENDPOINTS = {
-  REGISTER: `${API_URL}/auth/register`,
-  LOGIN: `${API_URL}/auth/login`,
-  EVENTS: `${API_URL}/events`,
-
-};
